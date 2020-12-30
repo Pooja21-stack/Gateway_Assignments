35f48b7cd7d9b87f610e384e5b5de3e7cb35b44b
_Pooja21-stack_ <poojamistry211098@gmail.com>
2020-12-30 13:24:52 +0530

    PreJoiningAssignment

---
+++ ProductManagement/sql_demo/sql_demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sql_demo.MvcApplication" Language="C#" %>
---
+++ ProductManagement/sql_demo/sql_demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sql_demo.Startup))]
+namespace sql_demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
