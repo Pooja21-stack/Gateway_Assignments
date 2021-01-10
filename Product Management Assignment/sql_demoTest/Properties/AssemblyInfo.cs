892f10854a1d354039038a2b934b039ed1120492
_Pooja21-stack_ <poojamistry211098@gmail.com>
2021-01-10 19:33:44 +0530

    Pre Joining Assignment

---
+++ Product Management Assignment/sql_demoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("sql_demoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sql_demoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1cca7af-8528-486c-8560-a3a94e1ec461")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Product Management Assignment/sql_demoTests/ProductControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sql_demo.Controllers;
+
+namespace sql_demoTests
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void Products()
+        {
+            // Arrange
+            //ProductController controller = new ProductController();
+
+            // Act
+           //ViewResult result = controller.ProductList() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+    }
+}
